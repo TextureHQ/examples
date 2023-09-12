20484261db44549f43cf432fc0c39a13d2518dcd
rcasto <richie@texture.energy>
2023-09-12 16:40:28 -0400

    switch javascript example to use package, add react as dev dep, yarn does not auto add react

--- connect/sdk/web/javascript/texture.js
+++ connect/sdk/web/javascript/texture.js
@@ -1,35 +1,37 @@
-const apiKey = import.meta.env.VITE_API_KEY
-const textureApiUrl = 'https://api.texture.energy'
+import { createSession } from "@texturehq/connect-sdk";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const textureApiUrl = "https://api.texture.energy";
 
 async function createLinkSession() {
   const response = await fetch(`${textureApiUrl}/v1/connections`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': apiKey,
+      "Content-Type": "application/json",
+      "x-api-key": apiKey,
     },
     body: JSON.stringify({
-      referenceId: 'connect-sdk-example',
-      clientName: 'Connect SDK Example',
+      referenceId: "connect-sdk-example",
+      clientName: "Connect SDK Example",
     }),
-  })
-  const responseJSON = await response.json()
-  return responseJSON.linkToken
+  });
+  const responseJSON = await response.json();
+  return responseJSON.linkToken;
 }
 
 export async function setupTexture(element) {
-  const linkToken = await createLinkSession()
-  const texture = window.Texture.createSession({
+  const linkToken = await createLinkSession();
+  const texture = createSession({
     linkToken,
     onSuccess: ({ scopedToken }) => {
-      console.log(scopedToken)
+      console.log(scopedToken);
     },
     onError({ type, reason }) {
-      console.error(type, reason)
+      console.error(type, reason);
     },
-  })
+  });
 
-  console.log(texture.url)
+  console.log(texture.url);
 
-  element.addEventListener('click', () => texture.open())
+  element.addEventListener("click", () => texture.open());
 }
