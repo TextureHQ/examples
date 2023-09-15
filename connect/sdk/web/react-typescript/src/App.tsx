c5379c952bdc6af402fe16ded275b4b2020f9dba
rcasto <richie@texture.energy>
2023-09-15 11:43:24 -0400

    update react example to use @texturehq/react-connect-sdk

--- connect/sdk/web/react-typescript/src/App.tsx
+++ connect/sdk/web/react-typescript/src/App.tsx
@@ -1,79 +1,25 @@
-import { useCreateSession } from "@texturehq/connect-sdk";
+import { useCreateConnectSession } from "@texturehq/react-connect-sdk";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useCallback, useEffect, useState } from "react";
-import { ICreateSessionResult, IConnectLink, IConnectLinkOptions } from "./schemas";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
-function useCreateLinkSession(connectApiKey: string): ICreateSessionResult {
-  const [link, setLink] = useState<IConnectLink | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-
-  const createSession = useCallback(
-    async function (options: IConnectLinkOptions): Promise<void> {
-      try {
-        setLoading(true)
-        setLink(null)
-        setError(null)
-
-        const response = await fetch(
-          `https://api.texture.energy/v1/connections`,
-          {
-            method: "POST",
-            headers: {
-              "x-api-key": connectApiKey,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(options),
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error(`Create Session Request failed: ${response.status} - ${response.statusText}`)
-        }
-
-        const responseJSON: IConnectLink = await response.json()
-        
-        setLink(responseJSON)
-        setLoading(false)
-      } catch (err) {
-        const error = err as Error
-        setError(error)
-        setLoading(false)
-      }
-    },
-    [connectApiKey]
-  );
-
-  return {
-    createSession,
-    link,
-    loading,
-    error,
-  }
-}
-
 function App() {
-  const { createSession, link, loading, error } = useCreateLinkSession(apiKey)
-  const { open } = useCreateSession({
-    linkToken: link?.linkToken || '',
+  const { open } = useCreateConnectSession({
+    connectApiKey: apiKey,
+    connectOptions: {
+      referenceId: "connect-sdk-example",
+      clientName: "Connect SDK Example",
+      tags: ["connect-sdk-example"],
+    },
     onSuccess: (session) => {
       console.log(session);
     },
-    onError: (error) => {
-      console.log(error);
+    onError: ({ type, reason }) => {
+      console.error(type, reason);
     },
   });
-
-  useEffect(() => {
-    createSession({
-      referenceId: "connect-sdk-example",
-      clientName: "Connect SDK Example",
-    })
-  }, [])
 
   return (
     <>
@@ -87,7 +33,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button disabled={loading || !!error} onClick={() => open()}>Open Texture</button>
+        <button onClick={() => open()}>Open Texture</button>
       </div>
     </>
   );
