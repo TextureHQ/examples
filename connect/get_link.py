368c395911af896ac8d4d0ed5c1447e57724336b
Victor Quinn <mail@victorquinn.com>
2023-09-27 11:24:53 -0400

    Update get_link.py to include tags

--- connect/get_link.py
+++ connect/get_link.py
@@ -4,13 +4,16 @@ import requests
 url = "https://api.texture.energy/v1/connections"
 
 # The header to include with the request
-headers = {"x-api-key": "<your-api-key-goes-here>", "Content-Type": "application/json"}
+headers = {
+    "x-api-key": "<your-secret-api-key-here>",
+    "Content-Type": "application/json",
+}
 
 # JSON body data
 data = {
     "referenceId": "9bb290c8-fe97-46ce-92a8-952823164277",  # A unique ID in your system for this connection
-    "redirectUrl": "http://localhost:8910",  # The URL to redirect to after the user has connected
-    "groupId": "cllh2buhk000008la0ln6cb80",  # The ID of the group to connect to
+    "redirectUrl": "http://localhost:8000",  # The URL to redirect to after the user has connected
+    "tags": ["banana", "strawberry"],  # The tags to group devices by
     "clientName": "Energy Demo",  # The name of your application
     "email": "person@gmail.com",  # The email address of the user to connect
 }
