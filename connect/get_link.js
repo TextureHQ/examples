39e71ec8e4bef21839331c809585e5f02f4618fd
rcasto <richie@texture.energy>
2024-03-15 15:52:01 -0400

    replace groupId with tags in get_link javascript example

--- connect/get_link.js
+++ connect/get_link.js
@@ -15,7 +15,7 @@ fetch("https://api.texture.energy/v1/connections", {
   body: JSON.stringify({
     referenceId: "9bb290c8-fe97-46ce-92a8-952823164277", // A unique ID in your system for this connection
     redirectUrl: "http://localhost:8910", // The URL to redirect to after the user has connected
-    groupId: "cllh2buhk000008la0ln6cb80", // The ID of the group to connect to
+    tags: ["banana", "strawberry"], // The tags to group devices by
     clientName: "Energy Demo", // The name of your application
     email: "person@gmail.com", // The email address of the user to connect
   }),
