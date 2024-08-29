440e2faebf00aec35df52e22e5cd4418edb0ef90
Victor Quinn <victor@texturehq.com>
2024-08-29 14:37:30 -0400

    Update get_link.js

--- connect/get_link.js
+++ connect/get_link.js
@@ -6,7 +6,7 @@ const apiKey = process.env.TEXTURE_API_KEY || "<your-api-key-goes-here>";
 console.log(`Using API key: ${apiKey}`);
 
 // Node v18 has fetch built in
-fetch("https://api.texture.energy/v1/connections", {
+fetch("https://api.texturehq.com/v1/connections", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
@@ -16,7 +16,7 @@ fetch("https://api.texture.energy/v1/connections", {
     referenceId: "9bb290c8-fe97-46ce-92a8-952823164277", // A unique ID in your system for this connection
     redirectUrl: "http://localhost:8910", // The URL to redirect to after the user has connected
     tags: ["banana", "strawberry"], // The tags to group devices by
-    clientName: "Energy Demo", // The name of your application
+    clientName: "Example App", // The name of your application, if omitted defaults to your Organization name
     customerInfo: {
       email: "example@example.com", // The email address of the user to connect
       phone: "5555555555", // The phone number of the user to connect
